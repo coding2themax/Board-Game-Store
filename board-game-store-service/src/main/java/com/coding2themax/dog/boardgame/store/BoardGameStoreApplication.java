dde1b68fa9fab30b520e42c30d4627b9d86cf9e5
max quinones <maximilian.quinones@gmail.com>
2024-11-27 06:17:45 -0700

    init

---
+++ board-game-store-service/src/main/java/com/coding2themax/dog/boardgame/store/BoardGameStoreApplication.java
@@ -0,0 +1,13 @@
+package com.coding2themax.dog.boardgame.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGameStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardGameStoreApplication.class, args);
+	}
+
+}
